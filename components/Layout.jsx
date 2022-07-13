16f3b09c758291a1722249251b795a6bf2b22abe
Zarivate <Zarivate1@gmail.com>
2022-07-12 21:44:13 -0500

    Added header and footer to home page.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 // This is the file that handles rendering the footer, navigation bar, and any content right in the middle
 const Layout = ({ children }) => (
@@ -8,12 +10,16 @@ const Layout = ({ children }) => (
       <title>Real Estate</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
-      <header>Navbar goes here later</header>
+      <header>
+        <Navbar />
+      </header>
       <main>
         {/* The children prop is equal to whatever is passed into the Layour component when it's called */}
         {children}
       </main>
-      <footer>Footer goes here</footer>
+      <footer>
+        <Footer />
+      </footer>
     </Box>
   </>
 );
