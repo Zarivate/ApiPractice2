68cf2f40122045161d4e9097539df4d0e56c78be
Zarivate <Zarivate1@gmail.com>
2022-07-13 23:18:16 -0500

    Finished image pages for each property with all details listed

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -29,6 +29,108 @@ const PropertyDetails = ({
 }) => (
   <Box maxWidth="1000px" margin="auto" p="4">
     {photos && <ImageScrollbar data={photos} />}
+    <Box w="full" p="6">
+      {/* Most of the below code is the same as what's in the Property.jsx file since the same items need to be displayed */}
+      <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center">
+          <Box paddingRight="3" color="green.400">
+            {isVerified && <GoVerified />}
+          </Box>
+          <Text fontWeight="bold" fontSize="lg">
+            AED {millify(price)}
+            {rentFrequency && `/${rentFrequency}`}
+          </Text>
+        </Flex>
+        <Box>
+          <Avatar size="sm" src={agency?.logo?.url} />
+        </Box>
+      </Flex>
+      <Flex
+        alignItems="center"
+        p="1"
+        justifyContent="space-between"
+        w="250px"
+        color="blue.400"
+      >
+        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
+        <BsGridFill />
+      </Flex>
+      {/* Slight adjustment here where the title is placed in it's own individual Box component  */}
+      <Box marginTop="2">
+        <Text fontSize="lg" marginBottom="2" fontWeight="bold">
+          {title}
+        </Text>
+        <Text lineHeight="2" color="gray.600">
+          {/* Because the description is just iregular text, it sadly can't be styled so is kept how it is even if it looks awkward */}
+          {description}
+        </Text>
+      </Box>
+      <Flex
+        flexWrap="wrap"
+        textTransform="uppercase"
+        justifyContent="space-between"
+      >
+        <Flex
+          justifyContent="space-between"
+          w="400px"
+          borderBottom="1px"
+          borderColor="gray.100"
+          p="3"
+        >
+          <Text>Type</Text>
+          <Text fontWeight="bold">{type}</Text>
+        </Flex>
+        <Flex
+          justifyContent="space-between"
+          w="400px"
+          borderBottom="1px"
+          borderColor="gray.100"
+          p="3"
+        >
+          <Text>Purpose</Text>
+          <Text fontWeight="bold">{purpose}</Text>
+        </Flex>
+        {furnishingStatus && (
+          <Flex
+            justifyContent="space-between"
+            w="400px"
+            borderBottom="1px"
+            borderColor="gray.100"
+            p="3"
+          >
+            <Text>Furnishing Status</Text>
+            <Text fontWeight="bold">{furnishingStatus}</Text>
+          </Flex>
+        )}
+      </Flex>
+      <Box>
+        {amenities.length && (
+          <Text fontSize="2xl" fontWeight="black" marginTop="5">
+            Amenities
+          </Text>
+        )}
+        <Flex flexWrap="wrap">
+          {amenities.map((item) =>
+            // Because some amentiies have sub amenities we have to map it twice
+            item.amenities.map((amenity) => (
+              <Text
+                // Together, all these styles work together to create a blue little card that separates each amenity
+                fontWeight="bold"
+                color="blue.400"
+                fontSize="l"
+                p="2"
+                bg="gray.200"
+                m="1"
+                borderRadius="5"
+                key={amenity.text}
+              >
+                {amenity.text}
+              </Text>
+            ))
+          )}
+        </Flex>
+      </Box>
+    </Box>
   </Box>
 );
 
