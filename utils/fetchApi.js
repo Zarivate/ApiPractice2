a4b4a7d3a113e1d9f91c491b303bc567ff4faebe
Zarivate <Zarivate1@gmail.com>
2022-07-25 23:25:07 -0500

    Removed redundant code

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchAPI = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": process.env.REACT_BAYUT_APIKEY,
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
